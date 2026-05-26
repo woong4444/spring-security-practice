6796a2e676354b7b443f7af9795f4823d714f5f4
jjang051 <tjjjang051@nate.com>
2026-05-26 14:41:17 +0900

    controller

--- first/src/main/java/com/jjang051/first/controller/HomeController.java
+++ first/src/main/java/com/jjang051/first/controller/HomeController.java
@@ -1,9 +1,14 @@
 package com.jjang051.first.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 //controller라는 annotation은 요청을 처리하는 곳이다.
 @Controller
@@ -47,5 +52,38 @@ public class HomeController {
                 </body>
                 </html>
                """.formatted(name,age);
+    }
+    @GetMapping("/user")
+    @ResponseBody
+    public Map<String,String> user() {
+        Map<String,String>  hashMap = new HashMap<>();
+        hashMap.put("userID","jjang051");
+        return hashMap;
+    }
+
+    @GetMapping("/template01")
+    public String template01(
+    @RequestParam(name="userID",required = true) String userID,
+    @RequestParam(name="userPW",required = true) String userPW,
+    Model model
+                             ) {
+        model.addAttribute("userID",userID);
+        model.addAttribute("userPW",userPW);
+        return "template01";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+    @PostMapping("/login")
+    public String loginProcess(@RequestParam(name="userID", required = true) String userID,
+                               @RequestParam(name="userPW", required = true) String userPW
+                               ) {
+        if(userID.equals("jjang051") && userPW.equals("1234")){
+            return "login-ok";
+        } else {
+            return "login-fail";
+        }
     }
 }
