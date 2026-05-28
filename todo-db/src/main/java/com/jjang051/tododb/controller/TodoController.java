d5057e5b30f385769e70ddeb8f9e994eeddb2852
jjang051 <tjjjang051@nate.com>
2026-05-28 14:15:46 +0900

    db연동

--- todo-db/src/main/java/com/jjang051/tododb/controller/TodoController.java
+++ todo-db/src/main/java/com/jjang051/tododb/controller/TodoController.java
@@ -81,5 +81,21 @@ public class TodoController {
         }
         return "redirect:/list";
     }
-
+    @PostMapping("/complete")
+    public String complete(@RequestParam(name="index", required = true) int index) {
+        String url = "jdbc:oracle:thin:@localhost:1521:xe";
+        String username = "spring";
+        String password = "1234";
+        String sql = "UPDATE todo SET  complete = 'Y' WHERE NO=?";
+        try {
+            Connection conn = DriverManager.getConnection(url,username,password);
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1,index);
+            int result = pstmt.executeUpdate();
+            System.out.println("result==="+result);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return "redirect:/list";
+    }
 }
