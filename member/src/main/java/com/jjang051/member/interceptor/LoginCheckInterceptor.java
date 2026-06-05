1abd8db8d413ee522b626d98f0edd4ccf50cf62d
jjang051 <tjjjang051@nate.com>
2026-06-05 13:14:32 +0900

    member interceptor

--- member/src/main/java/com/jjang051/member/interceptor/LoginCheckInterceptor.java
+++ member/src/main/java/com/jjang051/member/interceptor/LoginCheckInterceptor.java
@@ -6,6 +6,8 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 //요청을 처리하기 전에 중간에서 낚아채는 역할을 한다.
+//브라우져 => Interceptor => Controller => View
+//AoP   controller => AoP => Service
 public class LoginCheckInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request,
                              HttpServletResponse response,
