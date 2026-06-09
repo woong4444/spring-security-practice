14166cb4c47a0efea91b7c2dd31e1563dc8edce2
jjang051 <tjjjang051@nate.com>
2026-06-09 10:58:12 +0900

    naver toast ui editor

--- mybatis/src/main/java/com/jjang051/mybatis/board/controller/BoardController.java
+++ mybatis/src/main/java/com/jjang051/mybatis/board/controller/BoardController.java
@@ -2,18 +2,41 @@ package com.jjang051.mybatis.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
     @GetMapping("/write")
-    public String write(){
+    public String write() {
         return "board/write";
     }
+
     @PostMapping("/write")
     @ResponseBody
-    public String write(@RequestParam String content){
+    public String write(@RequestParam String content) {
         System.out.println(content);
         return "board/write";
+    }
+
+    @PostMapping("/upload")
+    @ResponseBody
+    public String upload(@RequestParam("upload") MultipartFile upload) {
+        String fileName = UUID.randomUUID() + "_" + upload.getOriginalFilename();
+        Path uploadPath = Paths.get("C:/upload/" + fileName);
+        try {
+            if (!Files.notExists(uploadPath)) {
+                Files.createDirectories(uploadPath);
+            }
+            Path saveFile =  uploadPath.resolve(fileName);
+            Files.copy(upload.getInputStream(), saveFile, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return "/upload/"+fileName;
     }
 }
